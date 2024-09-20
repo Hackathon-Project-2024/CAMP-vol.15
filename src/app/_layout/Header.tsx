4b791d4789f4b64b89a9e95d6bfea78d12e1a9a0
xBlackTea <xBlackTea.Official@gmail.com>
2024-09-21 02:13:38 +0900

    feature/#1 初期設定を完了

---
+++ src/app/_layout/Header.tsx
@@ -0,0 +1,12 @@
+import { Box, Typography } from '@mui/material';
+import React from 'react';
+
+export const Header = () => {
+  return (
+    <Box height="60px" paddingLeft="20px" bgcolor="#111">
+      <Typography color="white" fontSize="40px" fontWeight="bold">
+        Stellar
+      </Typography>
+    </Box>
+  );
+};
