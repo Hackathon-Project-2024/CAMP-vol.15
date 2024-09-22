1148b6d6ab4d4b2f762c8f327284d726074e5033
Hinata0607 <atahin0607@gmail.com>
2024-09-22 10:26:11 +0900

    add: husky test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,29 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 import { Michroma } from 'next/font/google';
-import { Header } from "./_layout/Header";
+import { Header } from './_layout/Header';
 
 const font = Michroma({ weight: '400', subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Stellar",
-  description: "オリジナル音声AI生成サイト",
+	title: 'Stellar',
+	description: 'オリジナル音声AI生成サイト',
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="ja">
-      <body className={font.className} style={{ margin: 0, padding: 0, boxSizing: 'border-box' }}>
-        <Header />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ja">
+			<body
+				className={font.className}
+				style={{ margin: 0, padding: 0, boxSizing: 'border-box' }}
+			>
+				<Header />
+
+				{children}
+			</body>
+		</html>
+	);
 }
